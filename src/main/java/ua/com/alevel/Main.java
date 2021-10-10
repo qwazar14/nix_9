b325c130e49753995eb3f3efcd117fc8f5cded81
qwazar14 <zenez89@gmail.com>
2021-10-10 17:22:59 +0300

    first try

---
+++ src/main/java/ua/com/alevel/Main.java
@@ -0,0 +1,12 @@
+package ua.com.alevel;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println(Main.class.getSimpleName());
+        System.out.println("1. Parsing string and getting the sum of all numbers");
+        System.out.println("2. Parsing string and getting the count of each letter");
+        System.out.println("3. Lessons timetable");
+        System.out.println("    Select task:");
+    }
+}
